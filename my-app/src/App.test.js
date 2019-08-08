d84e173bf4a37ec1ada4db3bf75679f1e99ca388
John <cjd8801@gmail.com>
2019-08-08 17:53:35 -0500

    2 test passed

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -7,3 +7,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', ()=> {
+  const label = document.createElement("label");
+  ReactDOM.render(<App/>,label)
+  ReactDOM.unmountComponentAtNode(label);
+})
